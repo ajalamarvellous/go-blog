c88dc40a3cdfd8e29598b558a4e372f07323aba4
shelby <ajalaoluwamayowa00@gmail.com>
2024-10-11 16:36:12 +0100

    Add sql database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 
@@ -19,14 +21,26 @@ func main(){
 	// define commandline arguments for port-address
 	// the arguments in order name_of_arg, default_value, description
 	addr := flag.String("addr", ":4000", "HTTP Port to use")
+	// Add a flag for the database manager
+    dsn := flag.String("dsn", "web:pass@/go_project?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
-	
+
 	// creating two different loggers for info and errors
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// instantiating the database pool 
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+    // Ensure every other function has ran before runinng db.Close
+    // but also db closes before the main file closes
+    defer db.Close()
+
 	// instantiating application class
-	app := application{
+	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
 	}
@@ -44,8 +58,20 @@ func main(){
 	// http.ListenAndServer requires majorly 2 parameters, port and router
 	app.infoLog.Printf("Starting server on %s", *addr)
 	// err := http.ListenAndServe(*addr, mux)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	// if any error (http.ListenAndServe returns non nil), log error and close
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+    err = db.Ping()
+    if err != nil {
+        return nil, err
+    }
+    return db, nil
 }
